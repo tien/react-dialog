2765cc74af7ed4557865664bbf529da8be9c4ee2
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-09 14:51:23 +1200

    feat: init

---
+++ src/Dialog.tsx
@@ -0,0 +1,68 @@
+import useMergedRef from '@react-hook/merged-ref';
+import React, {useEffect, useRef} from 'react';
+
+export type DialogProps = React.DetailedHTMLProps<
+  React.DialogHTMLAttributes<HTMLDialogElement>,
+  HTMLDialogElement
+> & {
+  isModal?: boolean;
+  onClickBackdrop?: () => unknown;
+};
+
+export const Dialog = React.forwardRef<HTMLDialogElement, DialogProps>(
+  // eslint-disable-next-line react/prop-types
+  function Dialog({open, isModal, onClickBackdrop, ...props}, ref) {
+    const innerRef = useRef<HTMLDialogElement>();
+    const mergedRef = useMergedRef(ref, innerRef);
+
+    useEffect(() => {
+      if (open) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (innerRef.current?.open === false) {
+          if (isModal) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            innerRef.current?.showModal();
+          } else {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            innerRef.current?.show();
+          }
+        }
+      } else {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (innerRef.current?.open) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          innerRef.current?.close();
+        }
+      }
+    }, [isModal, open]);
+
+    useEffect(() => {
+      const listener = function (this: HTMLDialogElement, event: MouseEvent) {
+        const rect = this.getBoundingClientRect();
+        if (
+          event.clientY < rect.top ||
+          event.clientY > rect.bottom ||
+          event.clientX < rect.left ||
+          event.clientX > rect.right
+        ) {
+          onClickBackdrop?.();
+        }
+      };
+
+      innerRef.current?.addEventListener('click', listener);
+
+      return innerRef.current?.removeEventListener('click', listener);
+    }, [onClickBackdrop]);
+
+    return (
+      <dialog ref={mergedRef} {...props} open={isModal ? undefined : open} />
+    );
+  }
+);
+
+export default Dialog;
